85a5f2b2434462231e6865b791db92f1845e9cef
Jailson Pinheiro Lima <jailsonp.lima03@gmail.com>
2023-02-25 17:23:45 -0300

    feat: add toggle nav bar function

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,1 +1,11 @@
 console.log("app start");
+
+const menu = document.querySelector(".m-menu");
+const nav = document.querySelector(".m-nav");
+
+function toggleMenuAndNav() {
+  menu.classList.toggle("is-active");
+  nav.classList.toggle("is-active");
+}
+
+menu.addEventListener("click", toggleMenuAndNav);
