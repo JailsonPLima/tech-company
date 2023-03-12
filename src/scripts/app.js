296db088788c573aacf893b8d6f599bc26cdb71e
Jailson Pinheiro Lima <jailsonp.lima03@gmail.com>
2023-03-12 19:40:10 -0300

    feat: add alert when clicking links

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,7 +1,6 @@
-console.log("app start");
-
 const menu = document.querySelector(".m-menu");
 const nav = document.querySelector(".m-nav");
+const links = [...document.querySelectorAll(".m-link")];
 
 function toggleMenuAndNav() {
   menu.classList.toggle("is-active");
@@ -9,3 +8,11 @@ function toggleMenuAndNav() {
 }
 
 menu.addEventListener("click", toggleMenuAndNav);
+
+function onFakeLinkClick() {
+  alert("Esse link é somente para demostração.\nThis link is for demo only.");
+}
+
+links.map((element) => {
+  element.addEventListener("click", onFakeLinkClick);
+});
